706771cb3816a6f4d359d2f2fd84fc45e12d282c
quynhhodev <quynh.dev2k1@gmail.com>
2025-11-12 13:55:43 +0700

    update code

---
+++ src/Controller/EnvDataController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EnvDataController extends AbstractController
+{
+    #[Route('/env/data', name: 'app_env_data')]
+    public function index(): JsonResponse
+    {
+        $a  = 0;
+        while($a < 999999999){
+            $a += 1;
+        }
+        
+        return new JsonResponse(['data' => $a, 200]);
+    }
+}
