380d62f5e09716b2d23c7acef5ab2779d4e1b7f7
quynhhodev <quynh.dev2k1@gmail.com>
2026-09-02 14:10:44 +0700

    log debug

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -7,20 +7,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Messenger\MessageBusInterface;
-use App\Entity\User;
-use App\Message\AMessage;
 use App\Repository\UserRepository;
+use Psr\Log\LoggerInterface;
 
 final class UsersController extends AbstractController
 {
     public function __construct(
-        private UserRepository $userRepo
+        private UserRepository $userRepo,
+        private LoggerInterface $logger
     ) {}
 
     #[Route('/users', name: 'app_users')]
     public function index(MessageBusInterface $bus, EntityManagerInterface $em): JsonResponse
     {
         $users = $this->userRepo->findAll();
+        $this->logger->debug("Client called Users");
 
         return new JsonResponse(['users' => $users], 200);
     }
